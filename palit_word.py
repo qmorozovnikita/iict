79dfdecff7d5f5ec442c369c7c11524fffe624a9
Nikita <nimokirotazov@gmail.com>
2017-12-15 12:32:43 +0700

    Create palit_word.py

---
+++ palit_word.py
@@ -0,0 +1,16 @@
+//Является ли слово палиндромом
+slovo = str(input())
+x = len(slovo)
+i = 0
+x = x - 1
+k = 0
+while x - i >= i:
+    if slovo[x - i] == slovo[i]:
+        i += 1
+    else:
+        k = 1
+        break
+if k == 1:
+  print("НЕ является")
+else:
+  print("является")

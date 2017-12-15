f69071bb07bfd0359819b53fd43de6da64c99c17
Nikita <nimokirotazov@gmail.com>
2017-12-15 12:50:35 +0700

    Create insert_com_toend.pas

---
+++ insert_com_toend.pas
@@ -0,0 +1,19 @@
+//Ввести имя файлап и изменить его расширение
+
+program qq;
+var s: string;
+    i,len: integer;
+begin
+     writeln('Ââåäèòå èìÿ ôàéëà');
+     readln(s);
+     len := Length(s) + 1 ;
+     INSERT('.exe',s,len);
+     writeln(s);
+
+
+
+
+
+writeln(s);
+end.
+
